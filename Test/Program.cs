e626d5906796528eff3a02421afe7a56944d0d22
Matt Nadareski <mnadareski@outlook.com>
2022-05-31 16:37:46 -0700

    Add dummy test executable

---
+++ Test/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using LibMSPackSharp;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                if (Directory.Exists(arg))
+                {
+                    foreach (string file in Directory.EnumerateFiles(arg))
+                    {
+                        ProcessFile(file);
+                    }
+                }
+                else if (File.Exists(arg))
+                {
+                    ProcessFile(arg);
+                }
+                else
+                {
+                    Console.WriteLine($"{arg} is not a file or folder, skipping...");
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Process a single file
+        /// </summary>
+        /// <param name="file">Path to a single file</param>
+        private static void ProcessFile(string file)
+        {
+            try
+            {
+                Console.WriteLine($"Path: {file}");
+
+                Console.WriteLine("Currently, this program does nothing. It will have reference implementations for all supported files in the future.")
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine();
+            }
+        }
+    }
+}
